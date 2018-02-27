b65576f217f3639c845ea9d4c970c935f0d72afa
Caius Citiriga <nbkmaxine@gmail.com>
2018-02-27 01:18:21 +0100

    testing dispatcher

---
+++ src/spec/test-dispatcher.spec.ts
@@ -0,0 +1,34 @@
+import { Dispatcher } from "../entities/dispatcher.entity";
+import { Command } from "../entities/command.entity";
+
+describe('Dispatcher', function () {
+    it('Should run the action on the dispatched command', () => {
+        //  Arrange
+        let itHasBeenCalled = false;
+        const command = new Command();
+        const dispatcher = new Dispatcher();
+
+        //  Act
+        command.setAction(() => itHasBeenCalled = true);
+        command.setName('cmd');
+        dispatcher.dispatch(command);
+
+        //  Assert
+        expect(itHasBeenCalled).toBeTruthy();
+    });
+
+    it('Should return the name of the command ran', () => {
+        //  Arrange
+        let itHasBeenCalled = false;
+        const command = new Command();
+        const dispatcher = new Dispatcher();
+
+        //  Act
+        command.setAction(() => itHasBeenCalled = true);
+        command.setName('cmd');
+        const result = dispatcher.dispatch(command);
+
+        //  Assert
+        expect(result).toBe(`Dispatching ${command.getName()}`);
+    });
+});
