30c87fe6a334a7d61fdce08d70edff44f1c87a5e
Caius Citiriga <nbkmaxine@gmail.com>
2018-05-14 20:19:31 +0200

    updated interface

--- dist/interfaces/plain/output.interface.d.ts
+++ dist/interfaces/plain/output.interface.d.ts
@@ -1,13 +1,13 @@
 import { ITableOpts } from "../opts/table-opts.interface";
 import { IKeyValuesOpts } from "../opts/key-values-opts.interface";
 export interface IOutput {
-    printInfo(text: string): void;
-    printTitle(text: string): void;
-    printError(text: string): void;
-    printMessage(text: string): void;
-    printWarning(text: string): void;
-    printSubtitle(text: string): void;
-    printBoxTitle(text: string): void;
-    printKeyValues(opts: IKeyValuesOpts): void;
-    printTableExperimental(table: ITableOpts): void;
+    printInfo(text: string, noColor?: boolean): void;
+    printTitle(text: string, noColor?: boolean): void;
+    printError(text: string, noColor?: boolean): void;
+    printMessage(text: string, noColor?: boolean): void;
+    printWarning(text: string, noColor?: boolean): void;
+    printSubtitle(text: string, noColor?: boolean): void;
+    printBoxTitle(text: string, noColor?: boolean): void;
+    printKeyValues(opts: IKeyValuesOpts, noColor?: boolean): void;
+    printTableExperimental(table: ITableOpts, noColor?: boolean): void;
 }
